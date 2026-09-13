c349318aec3fa7b57f86cf05d1c69625aa717c2d
Peter Moogk <peter_moogk@yahoo.com>
2026-09-12 23:52:25 -0400

    More setup changes

--- server/setup.bat
+++ server/setup.bat
@@ -1,8 +1,11 @@
 @echo on
-npm install
-npm run makecert
-npm run installclient
-npm run buildclient
-npm run build
+echo Starting to setup the square dancing application
+call npm install
+call npm run build
+call npm run makecert
+call npm run installclient
+call npm run buildclient
+call npm run build
+call npm run makecert
 
 echo If the previous command completed successfully, can now start the application by typing "npm start".
